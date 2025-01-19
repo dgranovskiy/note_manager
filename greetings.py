87a519ffb41006e8a1c211b26bd0efa54781a762
zxKILLMENxz <dengran3@gmail.com>
2025-01-19 16:24:03 +0300

    greetings.py

--- greetings.py
+++ greetings.py
@@ -5,3 +5,8 @@ status = "No completed"
 created_date = "31-12-2024"
 issue_date = "31-01-2025"
 print("Имя пользователя:", user_name)
+print(title)
+print(content)
+print(status)
+print(created_date)
+print(issue_date)
